9c9d047ec9b93c0fbcb514732f9efa6c25b4977d
ChetanSharan <142531059+ChetanSharan@users.noreply.github.com>
2024-09-06 14:45:24 +0530

    Update Access.py
    Altered the code to fit the standards.

--- Access.py
+++ Access.py
@@ -1,38 +1,68 @@
-#Step One: Fetch Stock Data
 import yfinance as yf
 import numpy as np
-aapl = yf.Ticker("AAPL")
-hist = aapl.history(period='2y',interval= "1h")
 
-#Step Two: Calculate Rolling Maximum and Minimum
-hist['max_price']= hist['Close'].rolling(window=20).max()
-hist['min_price']= hist['Close'].rolling(window=20).min()
+# Parameters
+ticker = 'AAPL'
+lookback = 20
 
-#Step Three: Determine Trading Signals
-hist['Short_signal']= hist['Close']>=hist['max_price']
-hist['Long_signal']= hist['Close']<=hist['min_price']
+"""
+Step One: Fetch Stock Data
+"""
+df_hist = yf.Ticker(ticker).history(period = '2y',interval = "1h")
 
-#Step Four: Signal Processing
-hist.loc[hist['Long_signal']==True,'Signal']=1
-hist.loc[hist['Short_signal']==True,'Signal']=-1
+"""
+Step Two: Calculate Rolling Maximum and Minimum
+"""
+df_hist['max_price'] = df_hist['Close'].rolling(window = lookback).max()
+df_hist['min_price'] = df_hist['Close'].rolling(window = lookback).min()
 
-#Step Five: Calculate Positions
-hist['Position0']=hist['Signal'].ffill()
-hist['Position1'] = hist['Position0'].diff()
-hist['Position1']= np.where(hist['Position1']!=0.0,hist['Signal'],0)
-hist['Position2']=hist['Position1']*hist['Close']
+"""
+Step Three: Determine Trading Signals
+"""
 
-#Step Six: Filtering and Return Calculation
-df_ret=hist[hist['Position1']!=0].copy()
-df_ret.dropna(subset=['Position2'],inplace=True)
-df_ret['abs_returns'] =  df_ret['Position2'].rolling(2).sum()
-df_ret.loc[::2,'abs_returns'] = '0'
-df_ret['abs_returns']=df_ret['abs_returns'].astype(float)
-df_ret['Position2_shifted'] = df_ret['Position2'].shift(1)
-df_ret['pct_returns'] = (df_ret['abs_returns'] / df_ret['Position2_shifted'])*100
-df_ret['pct_returns'] = df_ret['pct_returns'].fillna(0)
-df_ret = df_ret.drop(columns=['Position2_shifted'])
-df_ret['pct_returns_cumsum']=df_ret['pct_returns'].cumsum()
+# It was mentioned in the problem statement that when local max/min 'matches' the ‘Close’ value, the Short/Long signal should be generated.
+# However, wihh some knowledge of Finance & market behaviour, I have taken '<=' or '>=' for ‘matches’ rather than ‘=‘.
+# But if it needs to be ‘equals’, then just the sign needs to be changed.
 
-#Step Seven: Save the Results
+df_hist['Short_signal'] = df_hist['Close'] >= df_hist['max_price'] 
+df_hist['Long_signal'] = df_hist['Close'] <= df_hist['min_price']
+
+"""
+Step Four: Signal Processing
+"""
+df_hist['Signal'] = np.nan
+df_hist.loc[df_hist['Long_signal'] == True,'Signal'] = 1
+df_hist.loc[df_hist['Short_signal'] == True,'Signal'] = -1
+
+"""
+Step Five: Calculate Positions
+"""
+df_hist['Position0'] = df_hist['Signal'].ffill()
+
+# Here, its requested that I need to identify positions' initiation for Position1.
+# So, I have taken +1 & -1 that represets the positions' initiation for monetory calculations.
+position_diff = df_hist['Position0'].diff()
+df_hist['Position1'] = np.where(position_diff != 0.0, df_hist['Signal'], 0) 
+
+df_hist['Position2'] = df_hist['Position1'] * df_hist['Close']
+
+"""
+Step Six: Filtering and Return Calculation
+"""
+df_ret = df_hist[df_hist['Position1'] != 0].copy()
+
+df_ret.dropna(subset = ['Position2'], inplace = True)
+
+df_ret['abs_returns'] = df_ret['Position2'].rolling(2).sum()
+df_ret.loc[::2,'abs_returns'] = 0
+
+#df_ret['abs_returns']=df_ret['abs_returns'].astype(float)
+position2_shifted = df_ret['Position2'].shift(1)
+df_ret['pct_returns'] = ((df_ret['abs_returns'] / position2_shifted) * 100).fillna(0)
+
+df_ret['pct_returns_cumsum'] = df_ret['pct_returns'].cumsum()
+
+"""
+Step Seven: Save the Results
+"""
 df_ret.to_csv('aaa_returns.csv')
